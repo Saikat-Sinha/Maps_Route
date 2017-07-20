fe0bce7dfddef4610daf393e0d69a6f086d5f15f
AskSaikatSinha <saikat@myself.com>
2017-07-21 04:13:54 +0530

    Completed

--- index.js
+++ index.js
@@ -1,141 +1,164 @@
-//firebase Config
-var config = {
-    apiKey: "AIzaSyDSbvayl0GDnF6AcIp8lymbCz56tKAnIwg",
-    authDomain: "maps-route-1500324598753.firebaseapp.com",
-    databaseURL: "https://maps-route-1500324598753.firebaseio.com",
-    projectId: "maps-route-1500324598753",
-    storageBucket: "",
-    messagingSenderId: "150903818209"
-};
-firebase.initializeApp(config);
-
-/*Caching API key for Google maps */
-var API_key = "AIzaSyDHoaPyBOHcUQcck0-LEAw6AsVTMt7Wqho";
-/* Caching variable id from form*/
-var startingPoint = $('#startPt');
-/* Caching Waypoint add button */
-var wayPtBtn = $('#wayPt');
-
-
 /*Global variables:  Arrays to store future Geocode data*/
 var waysGeoCodeArr = [];
 var startGeoCodeArr = {};
 var destGeoCodeArr = {};
 
-function removeWays(i) {
-    waysGeoCodeArr.splice(i,1);
-    $('#'+ i).remove();
-    console.log(waysGeoCodeArr);
-}
-
-var database = firebase.database();
-var ref = database.ref('map_par');
-var data = {
-    WaysGeo: waysGeoCodeArr,
-    StartGeo: startGeoCodeArr,
-    DestGeo: destGeoCodeArr
-};
 
 
-/* add waypoints button click handler */
-wayPtBtn.click(function () {
-    var wayInp = $("#wayPtInput").val();
-    geoCode(wayInp, 'wayInput');
-});
-
-
-/*Template function for adding way points data in DOM*/
-function addWayPointsToHTML() {
-    $( ".point" ).remove();
-    for(var i=0; i<waysGeoCodeArr.length; i++){
-        $('.point-addr').after(`
-                    <li class="point" id=${i}>
-                        <span>${waysGeoCodeArr[i].location}</span>
-                        <button type="button" onclick="removeWays(${i})" class="btn btn-default btn-circle pull-right click-rem"><i class="glyphicon glyphicon-remove"></i></button>
-                    </li> `);
-    }
-
-}
-
-
-/* Maps Geo-code API function */
-function geoCode(address, geoCodeFor) {
-    $.ajax({
-        url: "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=" + API_key,
-        success: function (response) {
-            var formattedAddress =  response.results[0].formatted_address;     //Formatted Address to the Input
-            var newGeoObj = {
-                location: formattedAddress,
-                stopover: true
-            };
-            if(geoCodeFor === "startInput"){
-                startGeoCodeArr.lat = response.results[0].geometry.location.lat;
-                startGeoCodeArr.lng = response.results[0].geometry.location.lng;
-                startGeoCodeArr.location = formattedAddress;
-            }
-            if(geoCodeFor === "destInput"){
-                destGeoCodeArr.lat = response.results[0].geometry.location.lat;
-                destGeoCodeArr.lng = response.results[0].geometry.location.lng;
-                destGeoCodeArr.location = formattedAddress;
-            }
-            if(geoCodeFor === "wayInput"){
-                waysGeoCodeArr.push(newGeoObj);
-                addWayPointsToHTML();
-            }
-        }
-    });
-}
-
+    var removeWays = function(i) {
+        waysGeoCodeArr.splice(i,1);
+        $('#'+ i).remove();
+    };
 
 
 /*   Map initialization function  */
 function initMap() {
+    //firebase Config
+    var config = {
+        apiKey: "AIzaSyDSbvayl0GDnF6AcIp8lymbCz56tKAnIwg",
+        authDomain: "maps-route-1500324598753.firebaseapp.com",
+        databaseURL: "https://maps-route-1500324598753.firebaseio.com",
+        projectId: "maps-route-1500324598753",
+        storageBucket: "",
+        messagingSenderId: "150903818209"
+    };
+    firebase.initializeApp(config);
+
+    /*Caching API key for Google maps */
+    var API_key = "AIzaSyDHoaPyBOHcUQcck0-LEAw6AsVTMt7Wqho";
+    /* Caching variable id from form*/
+    var startingPoint = $('#startPt');
+    /* Caching Waypoint add button */
+    var wayPtBtn = $('#wayPt');
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
+
+
+    var database = firebase.database();
+    var ref = database.ref('map_par');
+    var data = {
+        WaysGeo: waysGeoCodeArr,
+        StartGeo: startGeoCodeArr,
+        DestGeo: destGeoCodeArr
+    };
+
+
+    ref.on('value', gotData, errData);
+    
+    function gotData(fireData) {
+        // console.log(fireData.val());
+        var Data = fireData.val();
+        var keys = Object.keys(Data);
+        // console.log(keys);
+        for(var i = 0; i<keys.length; i++){
+            var k = keys[i];
+            var DestGeo = Data[k].DestGeo;
+            var StartGeo = Data[k].StartGeo;
+            var WaysGeo = Data[k].WaysGeo;
+            // console.log(WaysGeo);
+
+            // $('#data_list').after('Start: '+ StartGeo.location +', Destination: '+DestGeo.location + ', WayPoints: '+ (WaysGeo[0].location == "undefined"? WaysGeo[0].location:""));
+        if(WaysGeo){
+            $('#data_list').after(`<a href="#" onclick="MyWindow = window.open('map.html?startGeoLat=${encodeURIComponent(StartGeo.lat)}&startGeoLng=${encodeURIComponent(StartGeo.lng)}&startGeoLoc=${StartGeo.location}&destGeoLat=${encodeURIComponent(DestGeo.lat)}&destGeoLng=${encodeURIComponent(DestGeo.lng)}&destGeoLoc=${encodeURIComponent(DestGeo.location)}&waysGeoLoc=${encodeURIComponent(WaysGeo.map(entry=>entry.location).join("."))}','MyWindow', width= 600, height=300)" class="rem-link"><li class="lis panel panel-info"><b>Start: </b>${StartGeo.location}<br> <b>Destination: </b>${DestGeo.location} <br><b>WayPoints: </b><br>${WaysGeo.map(entry=>entry.location).join(", <br>")}</li></a>`);
+        }
+        else{
+            $('#data_list').after(`<a href="#" onclick="MyWindow = window.open('map.html?startGeoLat=${encodeURIComponent(StartGeo.lat)}&startGeoLng=${encodeURIComponent(StartGeo.lng)}&startGeoLoc=${StartGeo.location}&destGeoLat=${encodeURIComponent(DestGeo.lat)}&destGeoLng=${encodeURIComponent(DestGeo.lng)}&destGeoLoc=${encodeURIComponent(DestGeo.location)}','MyWindow',width=600, height= 300)" class="rem-link"><li class="lis panel panel-info"><b>Start: </b>${StartGeo.location} <br><b>Destination: </b>${DestGeo.location}</a>`);
+        }
+        }
+    }
+
+    function errData(fireErr) {
+        console.log(fireErr);
+    }
+
+    /* add waypoints button click handler */
+    wayPtBtn.click(function () {
+        var wayInp = $("#wayPtInput").val();
+        geoCode(wayInp, 'wayInput');
+        console.log(waysGeoCodeArr);
+    });
 
     /*Adding submit event listener */
     startingPoint.submit(function (e) {
         e.preventDefault();
         var startInp = $('#startPtInput').val();
-        var destInp = $('#destPtInput').val();
         geoCode(startInp, 'startInput');
+        var destInp = $('#destPtInput').val();
         geoCode(destInp, 'destInput');
-        ref.push(data);
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
+        setTimeout(function () {
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+            ref.push(data);
+        },1000);
+
         console.log(startGeoCodeArr);
+        console.log(destGeoCodeArr);
     });
 
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
 
-    //map options
-    var options = {
-        zoom: 5,
-        center:{
-            lat: 20.5937,
-            lng: 78.9629
+    /*Template function for adding way points data in DOM*/
+    function addWayPointsToHTML() {
+        $( ".point" ).remove();
+        for(var i=0; i<waysGeoCodeArr.length; i++){
+            $('.point-addr').after(`
+                    <li class="point panel panel-default" id=${i}>
+                        <span>${waysGeoCodeArr[i].location}</span>
+                        <i onclick="removeWays(${i})" class="glyphicon glyphicon-remove btn-circle-sp pull-righ"></i>
+                    </li> 
+                    `);
         }
-    };
-    //new Map
-    var map = new google.maps.Map(document.getElementById('map'), options);
+    }
 
-    directionsDisplay.setMap(map);
+
+    /* Maps Geo-code API function */
+    function geoCode(address, geoCodeFor) {
+        $.ajax({
+            url: "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=" + API_key,
+            success: function (response) {
+                var formattedAddress =  response.results[0].formatted_address;     //Formatted Address to the Input
+                var newGeoObj = {
+                    location: formattedAddress,
+                    stopover: true
+                };
+                if(geoCodeFor === "startInput"){
+                    startGeoCodeArr.lat = response.results[0].geometry.location.lat;
+                    startGeoCodeArr.lng = response.results[0].geometry.location.lng;
+                    startGeoCodeArr.location = formattedAddress;
+                }
+                else if(geoCodeFor === "destInput"){
+                    destGeoCodeArr.lat = response.results[0].geometry.location.lat;
+                    destGeoCodeArr.lng = response.results[0].geometry.location.lng;
+                    destGeoCodeArr.location = formattedAddress;
+                }
+                else if(geoCodeFor === "wayInput"){
+                    waysGeoCodeArr.push(newGeoObj);
+                    addWayPointsToHTML();
+                }
+            }
+        });
+    }
+    console.log(waysGeoCodeArr);
+
 
 
     function calculateAndDisplayRoute(directionsService, directionsDisplay){
         var waypts = waysGeoCodeArr;
 
+        var startOriginPtslat = startGeoCodeArr.lat;
+        var startOriginPtslng = startGeoCodeArr.lng;
+        var destOriginPtslat = destGeoCodeArr.lat;
+        var destOriginPtslng = destGeoCodeArr.lng;
+
         directionsService.route({
-            origin: new google.maps.LatLng(startGeoCodeArr.lat, startGeoCodeArr.lng),
-            destination: new google.maps.LatLng(destGeoCodeArr.lat, destGeoCodeArr.lng),
+            origin: new google.maps.LatLng(startOriginPtslat, startOriginPtslng),
+            destination: new google.maps.LatLng(destOriginPtslat, destOriginPtslng),
             waypoints: waypts,
             optimizeWaypoints: true,
             travelMode: 'DRIVING'
         }, function (response, status) {
             if (status === 'OK') {
                 directionsDisplay.setDirections(response);
-                var route = response.routes[0];
-                for (var i = 0; i < route.legs.length; i++) {
-                    var routeSegment = i + 1;
-                }
             }
             else {
                 console.log('Directions request failed due to ' + status);
@@ -143,4 +166,56 @@ function initMap() {
         });
     }
 
+    //map options
+    var options = {
+        zoom: 5,
+        center:{
+            lat: 20.5937,
+            lng: 78.9629
+        },
+        styles: [
+            {
+                "stylers": [
+                    {
+                        "hue": "#2c3e50"
+                    },
+                    {
+                        "saturation": 250
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "lightness": 50
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            }
+        ]
+    };
+    //new Map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    var startPtInput = document.getElementById('startPtInput');
+    var destPtInput = document.getElementById('destPtInput');
+    var wayPtInput = document.getElementById('wayPtInput');
+    var autoComplete1 = new google.maps.places.Autocomplete(startPtInput);
+    var autoComplete2 = new google.maps.places.Autocomplete(destPtInput);
+    var autoComplete3 = new google.maps.places.Autocomplete(wayPtInput);
+
+    directionsDisplay.setMap(map);
 }
---
+++ new_map.js
@@ -0,0 +1,109 @@
+
+
+function initMap() {
+    var API_key = "AIzaSyDHoaPyBOHcUQcck0-LEAw6AsVTMt7Wqho";
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: {lat: 20.5937,
+            lng: 78.9629
+        },
+        styles: [
+        {
+            "stylers": [
+                {
+                    "hue": "#2c3e50"
+                },
+                {
+                    "saturation": 250
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": 50
+                },
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ]
+    });
+
+    directionsDisplay.setMap(map);
+    var url = document.location.href,
+        params = url.split('?')[1].split('&');
+    var startGeoLat = params[0].split('=')[1];
+    var startGeoLng = params[1].split('=')[1];
+    var startGeoLoc = decodeURIComponent(params[2].split('=')[1]);
+    var destGeoLat = params[3].split('=')[1];
+    var destGeoLng = params[4].split('=')[1];
+    var destGeoLoc = decodeURIComponent(params[5].split('=')[1]);
+
+    console.log(decodeURIComponent(params));
+
+    if(params[6] !== undefined){
+        console.log(decodeURIComponent(params[6]));
+        if(decodeURIComponent(params[6].split('=')[1]).split('.')){
+            var waysArr = decodeURIComponent(params[6].split('=')[1]).split('.');
+        }
+        else{
+            var waysArr = decodeURIComponent(params[6].split('=')[1]);
+        }
+
+        var waysGeo = [];
+        for(var j= 0; j< waysArr.length; j++){
+            var newGeoObj = {
+                location: waysArr[j],
+                stopover: true
+            };
+            waysGeo.push(newGeoObj);
+        }
+    }
+
+
+
+
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay){
+
+
+
+        var waypts = waysGeo;
+        var startOriginPtslat = startGeoLat;
+        var startOriginPtslng = startGeoLng;
+        var destOriginPtslat = destGeoLat;
+        var destOriginPtslng = destGeoLng;
+
+        directionsService.route({
+            origin: new google.maps.LatLng(startOriginPtslat, startOriginPtslng),
+            destination: new google.maps.LatLng(destOriginPtslat, destOriginPtslng),
+            waypoints: waypts,
+            optimizeWaypoints: true,
+            travelMode: 'DRIVING'
+        }, function (response, status) {
+            if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+            }
+            else {
+                console.log('Directions request failed due to ' + status);
+            }
+        });
+    }
+}
